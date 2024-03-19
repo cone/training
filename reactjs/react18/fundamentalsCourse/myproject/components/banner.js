353df4d17026cc239eed6760a667db812f52a8e7
Carlos Gutierrez <carlos.gutierrez@magmalabs.io>
2024-03-19 17:38:29 -0600

    Add component styles and inline styles

--- reactjs/react18/fundamentalsCourse/myproject/components/banner.js
+++ reactjs/react18/fundamentalsCourse/myproject/components/banner.js
@@ -1,12 +1,19 @@
 import Image from "next/image";
+import styles from "./banner.module.css";
+
+const subtitleStyle = {
+  fontStyle: "italic",
+  fontSize: "x-large",
+  color: "coral",
+};
 
 const Banner = () => {
   return (
     <header className="row mb-4">
       <div className="col-5">
-        <Image src="/generic.jpg" alt="logo" width="200" height="200"/>
+        <Image className={styles.logo} src="/generic.jpg" alt="logo" width="200" height="200"/>
       </div>
-      <div className="col-7 mt-5">
+      <div className="col-7 mt-5" style={subtitleStyle}>
         This is a test
       </div>
     </header>
