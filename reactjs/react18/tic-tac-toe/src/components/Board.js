64e43a256af27486ebf68ff7f120c65f286c8e80
Carlos Gutierrez <carlos.gutierrez@magmalabs.io>
2024-03-25 09:34:45 -0600

    Improve visuals

--- reactjs/react18/tic-tac-toe/src/components/Board.js
+++ reactjs/react18/tic-tac-toe/src/components/Board.js
@@ -3,29 +3,32 @@ import { findWinColumn, findWinDiagonal, findWinRow } from "../utils/winConditio
 import { findLeftDiagonal, findRightDiagonal } from "../utils/diagonals";
 import { getRowAndCol } from "../utils/board";
 
-const initialBoard = [
+const board = [
   ["", "", ""],
   ["", "", ""],
   ["", "", ""],
 ];
-const board = [...initialBoard];
 const leftDiagonal = findLeftDiagonal(board);
 const rightDiagonal = findRightDiagonal(board);
 
 const Board = () => {
   const [currentSymbol, setCurrentSymbol] = useState("O");
+  const [message, setMessage] = useState("");
+
 
   const renderButton = (position) => {
     return (
-      <button data-position={position} onClick={onButtonClick} className="btn cell"></button>
+      <button data-position={position} onClick={onButtonClick} className="cell"></button>
     );
   };
 
   const onButtonClick = (e) => {
-    e.target.innerText = currentSymbol;
     const [row, col] = getRowAndCol(e.target.dataset.position);
+    const selectedSymbol = board[row - 1][col - 1];
+    if (selectedSymbol) return;
+    e.target.innerText = currentSymbol;
     board[row - 1][col - 1] = currentSymbol;
-    if (findWinConditions(row, col)) alert(`${currentSymbol} won!!!`);
+    if (findWinConditions(row, col)) setMessage(`${currentSymbol} won!!!`);
     setCurrentSymbol(currentSymbol === "O" ? "X" : "O");
   };
 
@@ -39,26 +42,29 @@ const Board = () => {
   return (
     <div className="container text-center">
       <h1>Tic Tac Toe</h1>
-      <div className="row center-block">
-        <table className="board">
-          <tbody>
-            <tr>
-              <td>{renderButton("1-1")}</td>
-              <td>{renderButton("1-2")}</td>
-              <td>{renderButton("1-3")}</td>
-            </tr>
-            <tr>
-              <td>{renderButton("2-1")}</td>
-              <td>{renderButton("2-2")}</td>
-              <td>{renderButton("2-3")}</td>
-            </tr>
-            <tr>
-              <td>{renderButton("3-1")}</td>
-              <td>{renderButton("3-2")}</td>
-              <td>{renderButton("3-3")}</td>
-            </tr>
-          </tbody>
-        </table>
+      <h2>{message}</h2>
+      <div className="row">
+        <div>
+          <table className="board">
+            <tbody>
+              <tr>
+                <td>{renderButton("1-1")}</td>
+                <td>{renderButton("1-2")}</td>
+                <td>{renderButton("1-3")}</td>
+              </tr>
+              <tr>
+                <td>{renderButton("2-1")}</td>
+                <td>{renderButton("2-2")}</td>
+                <td>{renderButton("2-3")}</td>
+              </tr>
+              <tr>
+                <td>{renderButton("3-1")}</td>
+                <td>{renderButton("3-2")}</td>
+                <td>{renderButton("3-3")}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   )
