013e5a62df40af883eb18881115be2e6c0293b77
Carlos Gutierrez <carlos.gutierrez@magmalabs.io>
2024-04-10 17:07:47 -0600

    Refactor 'seat combinations' to reflect the real instructions

--- code_katas/seat_combinations.rb
+++ code_katas/seat_combinations.rb
@@ -1,25 +1,40 @@
-@combinations = [
-  [[1, 2], [1, 3]],
-  [[1, 2], [2, 4]],
-  [[1, 3], [3, 4]],
-  [[2, 4], [3, 4], [4, 6]],
-  [[3, 5], [5, 6], [5, 7]],
-  [[4, 6], [5, 6], [6, 8]],
-  [[5, 7], [7, 8]],
-  [[7, 8], [6, 8]],
-]
+# Print the possible combinations
+# | 1 | (2) |
+# | 3 | 4   |
+# | 5 | (6) |
+# | 7 | 8   |
+# () means is occupied
+# rules:
+# - next to each other
+# - above and below
+# - The fist element in the array is the number of seats
+#
+# seat_combinations([8, 2, 6])
 
-def seat_combinations(occupied)
-  result = []
-
-  @combinations.each do |value|
-    value.each do |inner|
-      result << inner if !(inner.intersection(occupied)).any?
+def seat_combinations(occupied_seats)
+  combinations = []
+  total_seats = occupied_seats.shift
+  total_seats.times do |seat_index|
+    seat = seat_index + 1
+    top_neighbour = seat - 2
+    bottom_neighbour = seat + 2
+    left_neighbour = seat - 1
+    right_neighbour = seat + 1
+    if top_neighbour >= 1 && !occupied_seats.include?(seat) && !occupied_seats.include?(top_neighbour)
+      combinations << [top_neighbour, seat]
+    end
+    if bottom_neighbour <= total_seats && !occupied_seats.include?(seat) && !occupied_seats.include?(bottom_neighbour)
+      combinations << [seat, bottom_neighbour]
+    end
+    if left_neighbour >= 1 && seat % 2 == 0 && !occupied_seats.include?(seat) && !occupied_seats.include?(left_neighbour)
+      combinations << [left_neighbour, seat]
+    end
+    if right_neighbour <= 8 && (seat - 1) % 2 == 0 && !occupied_seats.include?(seat) && !occupied_seats.include?(right_neighbour)
+      combinations << [seat, right_neighbour]
     end
   end
-
-  result.uniq
+  combinations.uniq
 end
 
-puts seat_combinations([2, 6]).inspect
+puts seat_combinations([8, 2, 6]).inspect
 # => [[1, 3], [3, 4], [3, 5], [5, 7], [7, 8]]
