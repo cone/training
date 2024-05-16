8f8dc6eab2632cc84a0dab4e06fb3dc781de1ab5
Carlos Gutierrez <carlos.gutierrez@magmalabs.io>
2024-05-16 11:18:18 -0600

    Organize ReactJS chagenges by bootstrapper

---
+++ reactjs/react18/chagenges/with_basic_app/src/components/App.js
@@ -0,0 +1,10 @@
+const App = () => (
+  <ul>
+    <li>Item 1</li>
+    <li>Item 2</li>
+    <li>Item 3</li>
+    <li>Item 4</li>
+  </ul>
+);
+
+export default App;
---
+++ reactjs/react18/chagenges/with_basic_app/src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom/client';
+import App from './components/App.js';
+
+const domRoot = document.getElementById('root');
+const root = ReactDOM.createRoot(domRoot);
+root.render(<App />);
